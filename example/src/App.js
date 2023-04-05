600a8eb4050eaf2443a37a987b7e7c2b1aa39df1
aslanliIbrahim <ibrahimra@code.edu.az>
2023-04-05 09:29:01 +0400

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'cartui'
+import 'cartui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
